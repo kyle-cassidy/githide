518523a2f732f0bbc14aee32b3fcead8f1b38dfa
kyle-cassidy <kiccassidy@icloud.com>
2024-05-17 02:03:04 -0400

    Initialized the tool by creating `.gitignore.local` and configuring git to use it.

--- githide/init_command.py
+++ githide/init_command.py
@@ -2,7 +2,7 @@ import os
 
 def init():
     """
-    Initializes the tool by creating `.gitignore.local` and configuring git to use it.
+    Initializes the tool by creating `.gitignore.local` and configures git to use it.
     """
     with open('.gitignore.local', 'w') as f:
         f.write('# Local gitignore file\n')
