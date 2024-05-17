c30bd7791668fd424c6621ba8217f0c1daa69af4
kyle-cassidy <kiccassidy@icloud.com>
2024-05-17 03:07:42 -0400

    Add logging statements to the add_command.py file

--- githide/add_command.py
+++ githide/add_command.py
@@ -1,6 +1,12 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def add(file):
     """
     Adds a file to the hidden list.
     """
+    logging.info(f'Adding {file} to .gitignore.local')
     with open('.gitignore.local', 'a') as f:
         f.write(f'{file}\n')
+    logging.info(f'{file} added to .gitignore.local')
