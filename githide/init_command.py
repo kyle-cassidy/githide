33307ec4b1a393b3a3b9edb3454ae86019b6ed30
kyle-cassidy <kiccassidy@icloud.com>
2024-05-17 02:03:07 -0400

    Added logging statements for file creation and git configuration in `init` function.

--- githide/init_command.py
+++ githide/init_command.py
@@ -1,10 +1,18 @@
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def init():
     """
     Initializes the tool by creating `.gitignore.local` and configures git to use it.
     """
+    logging.info('Creating .gitignore.local file')
     with open('.gitignore.local', 'w') as f:
+        f.write('# Local gitignore file\n')
+    logging.info('Configuring git to use .gitignore.local')
+    os.system('git config core.excludesfile .gitignore.local')
+    logging.info('Adding .gitignore.local to .git/info/exclude')
         f.write('# Local gitignore file\n')
     os.system('git config core.excludesfile .gitignore.local')
     with open('.git/info/exclude', 'a') as f:
