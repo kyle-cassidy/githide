93040d8a50c013b581cbabc2d889f9f524aeef3f
kyle-cassidy <kiccassidy@icloud.com>
2024-05-20 00:29:14 -0400

    Refactor list function name in githide/list_command.py

--- githide/cli.py
+++ githide/cli.py
@@ -1,7 +1,7 @@
 import argparse
 from githide.add_command import add
 from githide.init_command import init
-from githide.list_command import list as list_files
+from githide.list_command import list_files
 from githide.remove_command import remove
 from githide.undo_command import undo
 
